3de111e4c7d0c2621412da971be3617143fb0f30
Tibalt <anton.shheglov.1@gmail.com>
2019-04-17 22:44:30 +0300

    Version with 2 varies of calculation

--- EilerProject14.py
+++ EilerProject14.py
@@ -1,17 +1,31 @@
+def biggestCollatzSequenceV2(maxNumber):
+    itrList = []
+    for number in range(1, maxNumber):
+        iteration = 1
+        while (number != 1):
+            if number % 2 == 0:
+                number >>= 1
+            else:
+                number = 3 * number + 1
+            iteration += 1
+        itrList.append(iteration)
+    return max(itrList)
+
+
 def biggestCollatzSequence(maxNumber):
     higest = 0
     for number in range(1, maxNumber):
         copy = searchTheEnd(number, 1)
         if higest<copy:
             higest = copy
+    return higest
 
 def searchTheEnd(number, iteration):
-    while(number>1):
-        number = int(number)
+    while (number != 1):
         if number % 2 == 0:
-            number/=2
+            number >>= 1
         else:
-            number = 3*number + 1
+            number = 3 * number + 1
         iteration += 1
     return iteration
-biggestCollatzSequence(1000000)#не доделал
+print(biggestCollatzSequence(1000000))
